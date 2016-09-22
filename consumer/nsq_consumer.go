f5d70a9032512d0c3c3690d1bdbd58a783614c96
mike <mike@verystar.cn>
2016-09-22 15:12:59 +0800

    add nsq-consumer

---
+++ consumer/nsq_consumer.go
@@ -0,0 +1,112 @@
+package consumer
+
+import (
+	"fmt"
+	"github.com/nsqio/go-nsq"
+	"os"
+	"sync"
+	"time"
+)
+
+func dealMsg(inChan chan *nsq.Message) {
+	for msg := range inChan {
+		err := doWork(msg)
+		if err != nil {
+			msg.Requeue(-1)
+			continue
+		}
+		msg.Finish()
+	}
+}
+
+func doWork(msg *nsq.Message) error {
+	fmt.Println(string(msg.Body))
+	return nil
+}
+
+func checkStrEmpty(args ...string) (int, bool) {
+	for k, val := range args {
+		if 0 == len(val) {
+			return k, false
+		}
+	}
+	return -1, true
+}
+
+func checkIntZero(args ...int) (int, bool) {
+	for k, val := range args {
+		if 0 > val {
+			return k, false
+		}
+	}
+
+	return -1, true
+}
+
+//desc: universal consumer msg function
+func ConsumerMsg(topicName, channelName, lookupAddr string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	if k, ok := checkStrEmpty(topicName, channelName, lookupAddr); !ok {
+		fmt.Printf("args[%d] is null", k)
+		os.Exit(1)
+	}
+	inChan := make(chan *nsq.Message)
+	conf := nsq.NewConfig()
+	conf.Set("max-in-flight", 1000)
+	consumer, err := nsq.NewConsumer(topicName, channelName, conf)
+	if err != nil {
+		fmt.Println("[Error NewConsumer]", err)
+		panic(err)
+	}
+
+	consumer.AddHandler(nsq.HandlerFunc(func(m *nsq.Message) error {
+		inChan <- m
+		return nil
+	}))
+
+	if err := consumer.ConnectToNSQLookupd(lookupAddr); err != nil {
+		fmt.Println("[Error ConnectToNSQLookupd]", err)
+		panic(err)
+	}
+
+	go dealMsg(inChan)
+	// read from this channel to block until consumer is cleanly stopped
+	<-consumer.StopChan
+}
+
+//desc: consumer the given number msg
+//@return  the total exec_time of consumer all msg.
+func ConsumerGMsg(topicName, channelName, lookupAddr string, msgNum int) (exec_time float64) {
+	if k, ok := checkStrEmpty(topicName, channelName, lookupAddr); !ok {
+		fmt.Printf("args[%d] is null", k)
+		os.Exit(1)
+	}
+	if k, ok := checkIntZero(msgNum); !ok {
+		fmt.Printf("args[%d] is null", k)
+		os.Exit(1)
+
+	}
+	wg := &sync.WaitGroup{}
+	wg.Add(msgNum)
+
+	start := time.Now()
+	conf := nsq.NewConfig()
+	consumer, _ := nsq.NewConsumer(topicName, channelName, conf)
+	consumer.AddHandler(nsq.HandlerFunc(func(msg *nsq.Message) error {
+		if err := doWork(msg); err != nil {
+			//			wg.Done()
+			return err
+		}
+		wg.Done()
+		return nil
+	}))
+
+	if err := consumer.ConnectToNSQLookupd(lookupAddr); err != nil {
+		fmt.Println("[Error ConnectToNSQLookupd]", err)
+		panic(err)
+	}
+	wg.Wait()
+	exec_time = time.Since(start).Seconds()
+	return exec_time
+
+}
---
+++ consumer_main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"github.com/researchlab/nsq_demo/consumer"
+	"sync"
+)
+
+var (
+	TOPICNAME_LIST   = []string{"p_events", "s_events"}
+	CHANNELNAME_LIST = []string{"encode", "decode", "dev1.0-info"}
+)
+
+var (
+	TOPICNAME   = "s_events"
+	CHANNELNAME = "getInfo"
+	LOOKUPADDR  = "127.0.0.1:4161" //连接nsqlookupd 服务发现暴露的http ip:port
+)
+
+func main() {
+	consumerGMsg()
+	//consumerMsg()
+}
+
+//desc 发起 TOPICNAME_LIST * CHANNELNAME_LIST 个消费者,
+//只有主动中止或异常中止，否在这些消费者一直消费消息
+//直到消费完所有消息，并等着消费即将进入的消息
+func consumerMsg() {
+	var wg sync.WaitGroup
+	wg_len := len(TOPICNAME_LIST) * len(CHANNELNAME_LIST)
+	fmt.Println("wg_len:", wg_len)
+	wg.Add(wg_len)
+	for _, topicName := range TOPICNAME_LIST {
+		for _, channelName := range CHANNELNAME_LIST {
+			go consumer.ConsumerMsg(topicName, channelName, LOOKUPADDR, &wg)
+		}
+	}
+	wg.Wait()
+}
+
+//@return 返回消费给定数目消息的执行时间
+func consumerGMsg() {
+	msg_Num := 10000
+
+	exec_time := consumer.ConsumerGMsg(TOPICNAME, CHANNELNAME, LOOKUPADDR, msg_Num)
+
+	fmt.Printf("%d consumer exec_time: %f s", msg_Num, exec_time)
+}
